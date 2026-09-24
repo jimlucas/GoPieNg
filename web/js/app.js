f3e71bad665d24ff38f1914d2a84dcc8418e64f3
James Lucas III <github@cmsws.com>
2026-09-24 11:14:02 -0400

    Unify frontend module cache version

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,7 @@
-import { ChangeTracker, compareAddresses } from './refresh.js?v=11'
-import { api, auth } from './api.js?v=11'
-import { store } from './store.js?v=11'
-import { mountApp, setResetScroll } from './components.js?v=11'
+import { ChangeTracker, compareAddresses } from './refresh.js?v=12'
+import { api, auth } from './api.js?v=12'
+import { store } from './store.js?v=12'
+import { mountApp, setResetScroll } from './components.js?v=12'
 
 const root = document.getElementById('app')
 
